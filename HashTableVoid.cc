6d7c4842846a130cb869226d39add339e0504251
yshiroya <yshiroya@purdue.edu>
2015-04-07 02:54:28 -0400

    Make IRCServer

--- HashTableVoid.cc
+++ HashTableVoid.cc
@@ -63,8 +63,8 @@ bool HashTableVoid::find( const char * key, void ** data)
 	int h = hash(key);
 	HashTableVoidEntry * he = _buckets[h];
 	while(he != NULL) {
-		if(!strcmp(he->_key, key)) {
-			he -> _data  = data;
+		if(strcmp(he->_key, key) == 0) {
+			*data  = he->_data;
 			return true;
 		} 
 		he = he -> _next;
