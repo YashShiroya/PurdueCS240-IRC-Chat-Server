1bab2f7745cb9606b62d8de7b6d6a5dc5d859f89
yshiroya <yshiroya@purdue.edu>
2015-04-12 03:36:26 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -381,6 +381,7 @@ int IRCServer::find_s_users( s_users user_array[1000],const char * user) {
 	int i = 0;
 	while(i < MAX_USERS) {
 		if(strcmp(user_array[i].s_username,user) == 0) {
+			printf("strcmp %d\n",strcmp(user_array[i].s_username,user));
 			return 1;
 		}
 		i++;
