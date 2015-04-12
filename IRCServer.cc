c4790501e671a704975d4bfc8e326e41caca995f
yshiroya <yshiroya@purdue.edu>
2015-04-11 22:03:29 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -240,8 +240,11 @@ void IRCServer::processRequest( int fd )
 		if(space_encountered >= 3) {
 			argz[j++] = a; argz[j] = 0;
 		}
-
-
+		int k = 0;
+		while(k < strlen(argz) - 1) {
+			argz[k] = argz[k+1];
+		}
+		argz[k+1] = '\0';
 		i++;
 	}
 
