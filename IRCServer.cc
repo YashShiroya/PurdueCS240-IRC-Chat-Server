ef9f1e402d492b582b11700e64bc1bebc7a73541
yshiroya <yshiroya@purdue.edu>
2015-04-14 01:07:09 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -355,7 +355,7 @@ IRCServer::initialize()
 	//Initialize userpass
 	while(j < 100) {
 		userpass[j] = (char*) malloc(sizeof(char) * 100);
-		//userpass[j] = "A";
+	//	*userpass[j] = "";
 		j++;
 	}
 
@@ -457,10 +457,9 @@ void IRCServer::addUser(int fd, const char * user, const char * password, const 
 	int i = 0;
 	file = fopen("password.txt","a");
 	//Rooms
-	while(i < 100) {
-		//printf("\nuname value %s, userIn %s\n",uname(userpass[i]),user);
+	printf("\nnumber_users %s\n",number_users);
+	while(i < number_users) {
 		if(strcmp(uname(userpass[i]),user) == 0) {
-		//	printf("\nuname value %s, userIn %s\n",uname(userpass[i]),user);
 			const char * m =  "DENIED\r\n";
 			write(fd, m, strlen(m));
 			return;
