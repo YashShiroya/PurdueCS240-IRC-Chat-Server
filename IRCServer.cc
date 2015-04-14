e1f2e7bfcbb901a4727e997dd6874af1e6b8b7de
yshiroya <yshiroya@purdue.edu>
2015-04-14 00:59:06 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -354,8 +354,8 @@ IRCServer::initialize()
 
 	//Initialize userpass
 	while(j < 100) {
-		//userpass[j] = (char*) malloc(sizeof(char) * 100);
-		userpass[j] = "A";
+		userpass[j] = (char*) malloc(sizeof(char) * 100);
+		//userpass[j] = "A";
 		j++;
 	}
 
