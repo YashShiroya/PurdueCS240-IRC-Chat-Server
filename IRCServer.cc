302ce462f2c3fec65912c403980a19a255d4073a
yshiroya <yshiroya@purdue.edu>
2015-04-19 05:58:53 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -740,7 +740,7 @@ IRCServer::getMessages(int fd, const char * user, const char * password, const c
 			
 		int x = atoi(token);
 		if(check == 0) {write_client(fd,"NO SUCH ROOM2\r\n");}
-		if(x >= rooms[i].msg_num) {write_client(fd,"NO-NEW-MESSAGES\r\n");}
+		if(x >= rooms[i].msg_num) {write_client(fd,"NO-NEW-MESSAGES\r\n\r\n");}
 		char * str = (char *) malloc(sizeof(char) * 100);
 		sprintf(str,"msg_num %d\n",rooms[i].msg_num);
 		
